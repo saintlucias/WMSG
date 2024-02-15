7750081a4e061885e81460eeba857c1ec172cc81
saintlucias <saintlucias@naver.com>
2024-02-15 17:01:33 +0900

    iframe test commit

--- src/pages/MostPick.tsx
+++ src/pages/MostPick.tsx
@@ -14,18 +14,73 @@ const MostPick: React.FC = () => {
                 <CenterSpan>가장 선호하는 기능 5가지</CenterSpan>
             <PositionContainer>
                 <TabContainer>
-                    <Tab active={activeTab === 1} onClick={() => handleTabClick(1)}>프로젝트</Tab>
-                    <Tab active={activeTab === 2} onClick={() => handleTabClick(2)}>업무관리</Tab>
-                    <Tab active={activeTab === 3} onClick={() => handleTabClick(3)}>메신저</Tab>
-                    <Tab active={activeTab === 4} onClick={() => handleTabClick(4)}>히스토리</Tab>
-                    <Tab active={activeTab === 5} onClick={() => handleTabClick(5)}>목표</Tab>
+                    <Tab active={activeTab === 1} onClick={() => handleTabClick(1)}>감스트</Tab>
+                    <Tab active={activeTab === 2} onClick={() => handleTabClick(2)}>적재 - 새벽통화</Tab>
+                    <Tab active={activeTab === 3} onClick={() => handleTabClick(3)}>힛뜨TV</Tab>
+                    <Tab active={activeTab === 4} onClick={() => handleTabClick(4)}>블루캡슐</Tab>
+                    <Tab active={activeTab === 5} onClick={() => handleTabClick(5)}>참돔 키우기</Tab>
                 </TabContainer>
                 <Content>
-                    {activeTab === 1 && <p>테스트 탭 1 인뎅</p>}
-                    {activeTab === 2 && <p>집에</p>}
-                    {activeTab === 3 && <p>가고싶어요</p>}
-                    {activeTab === 4 && <p>개발시러</p>}
-                    {activeTab === 5 && <p>쒸부랄</p>}
+                    {
+                        activeTab === 1 && 
+                            <iframe 
+                                width="100%" 
+                                height="600px" 
+                                src="https://www.youtube.com/embed/2hpgR9hxEPw?autoplay=1" 
+                                title="감스트 손흥민vs이강인 싸웠다? 축협의 시선돌리기 역겹습니다.." 
+                                frameBorder="0" 
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                                allowFullScreen
+                            />
+                    }
+                    {
+                        activeTab === 2 && 
+                            <iframe 
+                            width="100%" 
+                            height="600px" 
+                            src="https://www.youtube.com/embed/XjdAJ5Oy-AQ?autoplay=1" 
+                            title="Midnight Call" 
+                            frameBorder="0" 
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                            allowFullScreen 
+                        />
+                    }
+                    {
+                        activeTab === 3 && 
+                            <iframe 
+                                width="100%" 
+                                height="600px" 
+                                src="https://www.youtube.com/embed/f_OtDKCf-l4?autoplay=1" 
+                                title="도심속 하천에 이런 게...!? 생각지도 못했습니다" 
+                                frameBorder="0" 
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                                allowFullScreen 
+                            />
+                    }
+                    {
+                        activeTab === 4 && 
+                            <iframe 
+                                width="100%" 
+                                height="600px" 
+                                src="https://www.youtube.com/embed/IMADQ0jqCtg?autoplay=1" 
+                                title="블루캡슐 배정인 이사 인터뷰" 
+                                frameBorder="0" 
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                                allowFullScreen 
+                            />
+                    }
+                    {
+                        activeTab === 5 && 
+                            <iframe 
+                                width="100%" 
+                                height="600px" 
+                                src="https://www.youtube.com/embed/8lYXx4_CUEg?autoplay=1" 
+                                title="1000배로 커지는 초대형어 참돔을 집에서 7개월간 키우면 생기는 일... ㄷㄷ" 
+                                frameBorder="0" 
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                                allowFullScreen 
+                            />
+                    }
                 </Content>
             </PositionContainer>
         </Container>
@@ -66,7 +121,7 @@ const Content = styled.div`
     width:auto;
     height:auto;
     margin-top:20px;
-    border: 3px solid black;
+
 `;
 
 const CenterSpan = styled.span`
